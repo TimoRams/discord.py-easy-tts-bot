ed3b32feb12798ea4ba0a888fd57e9ed999200e8
TimoRams <66584994+TimoRams@users.noreply.github.com>
2021-04-16 14:42:31 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import asyncio as asyncio
+import discord
+from discord.ext import commands
+from gtts import gTTS
+from mutagen.mp3 import MP3
+import os
+
+bot = commands.Bot(command_prefix='.', description="This is Easy tts bot")
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+@bot.command()
+async def sum(ctx, numOne: int, numTwo: int):
+    await ctx.send(numOne + numTwo)
+@bot.command()
+async def tts(ctx, *, ttsword):
+    os.remove("output.mp3")
+    language = 'de'
+    output = gTTS(text=ttsword, lang=language, slow=False)
+    output.save("output.mp3")
+    audio = MP3("output.mp3")
+    guild = ctx.guild
+    voice = await ctx.message.author.voice.channel.connect()
+    voice.play(discord.FFmpegPCMAudio('output.mp3'))
+    counter = 0
+    duration = audio.info.length  # In seconds
+    while not counter >= duration:
+        await asyncio.sleep(1)
+        counter = counter + 1
+    await voice.disconnect()
+# Events
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Streaming(name="Tutorials", url="http://www.twitch.tv/TimoRams"))
+    print('My Ready is Body')
+bot.run('TOKEN')
